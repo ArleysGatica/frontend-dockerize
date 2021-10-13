d5a9eda861e5cd965267bec4734491991151b397
ArleysGatica <gatica.arleys16@gmail.com>
2021-10-12 22:11:49 -0600

    S_C

--- src/menu.jsx
+++ src/menu.jsx
@@ -3,13 +3,11 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
-import Header from "../../../../components/header/header";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
-import ChromeReaderModeIcon from "@material-ui/icons/ChromeReaderMode";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import Badge from "@material-ui/core/Badge";
 
@@ -57,7 +55,7 @@ const Menu = (props) => {
 
   return (
     <>
-      <Header />
+    
       <div className={classes.bar}>
         <h1 className={classes.tittle}>{tittle}</h1>
         <Button size="small">
